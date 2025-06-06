781f002effd3d1c282c5e989829ea9622e91c87f
CoolName <78220549+VeryVeryCoolName@users.noreply.github.com>
2025-06-06 18:52:56 +0300

    Settings implementation

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -6,7 +6,7 @@ func _on_play_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
 
 
 func _on_quit_pressed() -> void:
