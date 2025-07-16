c2ea8e865476dd8fdd25b5d4e6d8baac8f58632b
VeryVeryCoolName <teo.circiumarescu@gmail.com>
2025-07-16 21:08:18 +0300

    Un inceput bun

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/test_scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 
 func _on_settings_pressed() -> void:
