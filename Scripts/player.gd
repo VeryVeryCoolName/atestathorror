e5654559f510ebcb1ebd42c1214991a2d08ae127
VeryVeryCoolName <teo.circiumarescu@gmail.com>
2025-07-18 20:29:08 +0300

    Update player.gd

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,12 @@
 class_name PlayerCharacter extends CharacterBody2D
+
 var movespeed : float = 65000.0
+var cardinaldirection : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO 
+
 func _ready():
 	pass
 func _process(delta):
-	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
 	direction.y = Input.get_action_strength("MoveDown") - Input.get_action_strength("MoveUp")
 	velocity = direction.normalized() * movespeed * delta
