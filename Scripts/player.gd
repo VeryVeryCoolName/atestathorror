fff7fc90205376f89ff0f3bd2706f2309e691650
PepeTheDog <61550581+PepeTheDog@users.noreply.github.com>
2025-07-18 19:41:21 +0300

    normalizat movement in diagonala

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,7 +6,8 @@ func _process(delta):
 	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
 	direction.y = Input.get_action_strength("MoveDown") - Input.get_action_strength("MoveUp")
-	velocity = direction * movespeed * delta
+	velocity = direction.normalized() * movespeed * delta
 	pass
 func _physics_process(delta):
 	move_and_slide()
+	pass
