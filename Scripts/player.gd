cd7349bf5c01e66fd7cbb48127868418b2824837
VeryVeryCoolName <teo.circiumarescu@gmail.com>
2025-07-18 16:59:26 +0300

    Update player.gd

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,12 @@
-class_name PlayerCharacter extends Node
+class_name PlayerCharacter extends CharacterBody2D
+var movespeed : float = 100.0
 func _ready():
 	pass
 func _process(delta):
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
+	direction.y = Input.get_action_strength("MoveDown") - Input.get_action_strength("MoveUp")
+	velocity = direction * movespeed * delta
 	pass
+func _physics_process(delta):
+	move_and_slide()
