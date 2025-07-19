6a1d192ac94bad2f1c179c84813591520c550bbf
VeryVeryCoolName <teo.circiumarescu@gmail.com>
2025-07-19 19:30:13 +0300

    attack wip

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,14 @@
 class_name PlayerCharacter extends CharacterBody2D
 
+@export var offset = 32
 @export var movespeed : float = 65000.0
 var cardinaldirection : Vector2 = Vector2.DOWN
 var direction : Vector2 = Vector2.ZERO 
 var is_attacking: bool = false
-
 @onready var attack_area = $AttackArea
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _process(delta):
-	
 	if not is_attacking:
 		direction.x = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
 		direction.y = Input.get_action_strength("MoveDown") - Input.get_action_strength("MoveUp")
@@ -27,9 +27,26 @@ func _physics_process(delta):
 
 func attack():
 	is_attacking = true
-	velocity = Vector2.ZERO  
-	$AttackArea.monitoring = true
-	$AttackArea.position = cardinaldirection * 16  
-	await get_tree().create_timer(0.2).timeout  
-	$AttackArea.monitoring = false
+	velocity = Vector2.ZERO
+	var original_position = attack_area.position  
+	match cardinaldirection:
+		Vector2.UP:
+			animated_sprite.play("attack_up")
+			attack_area.position = Vector2(0, -offset)
+		Vector2.DOWN:
+			animated_sprite.play("attack_down")
+			attack_area.position = Vector2(0, offset)
+		Vector2.LEFT:
+			animated_sprite.play("attack_left")
+			attack_area.position = Vector2(-offset, 0)
+		Vector2.RIGHT:
+			animated_sprite.play("attack_right")
+			attack_area.position = Vector2(offset, 0)
+
+	attack_area.monitoring = true
+
+	await get_tree().create_timer(0.2).timeout
+
+	attack_area.monitoring = false
+	attack_area.position = original_position
 	is_attacking = false
