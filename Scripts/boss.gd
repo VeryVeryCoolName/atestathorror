3383c7ae04a61a73f61fd7c555de427fc8470c6c
PepeTheDog <61550581+PepeTheDog@users.noreply.github.com>
2025-07-21 16:53:59 +0300

    boss attack prototype

--- Scripts/boss.gd
+++ Scripts/boss.gd
@@ -2,23 +2,29 @@ extends CharacterBody2D
 
 var state = "idle"
 var player = null
+var distanceToPlayer = 1000
 var moveDirection = Vector2()
 var moveSpeed = 10000
 
-func _process(_delta):
-	if !player && is_inside_tree():
+func _ready():
+	if is_inside_tree():
 		player = get_parent().get_node("PlayerCharacter")
+
+func _process(_delta):
 	if state == "idle" and player:
 		state = "move"
 	if state == "move":
-		#if distanceToPlayer() < 50:
-			#state = "attack"
 		var playerPosition = player.position
 		moveDirection.x = playerPosition.x - position.x
 		moveDirection.y = playerPosition.y - position.y
-		print(moveDirection.x)
-		print(moveDirection.y)
+		if moveDirection.length() < 200:
+			state = "attack"
+	if state == "attack":
+		attack()
+		state = "cooldown"
 	if state == "cooldown":
+		moveDirection = Vector2.ZERO
+		await get_tree().create_timer(0.5).timeout
 		state = "idle"
 	pass
 
@@ -27,5 +33,6 @@ func _physics_process(delta):
 	move_and_slide()
 	pass
 
-func distanceToPlayer():
+func attack():
+	print("attack")
 	pass
