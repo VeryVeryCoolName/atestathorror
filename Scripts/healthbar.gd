88c36b99a97eaed5bf7c57d76a43e929a8f461fa
VeryVeryCoolName <teo.circiumarescu@gmail.com>
2025-07-22 13:40:12 +0300

    Health Bar UPDATE xoxo

---
+++ Scripts/healthbar.gd
@@ -0,0 +1,20 @@
+extends ProgressBar
+
+@export var max_health := 100
+@export var current_health := 100
+
+func _ready():
+	update_bar()
+
+func update_health(new_health: int):
+	current_health = clamp(new_health, 0, max_health)
+	update_bar()
+
+func set_max_health(new_max: int):
+	max_health = new_max
+	current_health = min(current_health, max_health)
+	update_bar()
+
+func update_bar():
+	max_value = max_health
+	value = current_health
