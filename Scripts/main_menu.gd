21f4b80ae509b35859e10f40888951660c669913
VeryVeryCoolName <78220549+VeryVeryCoolName@users.noreply.github.com>
2025-11-05 09:39:29 +0200

    Credits menu (nefunctional)

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -10,6 +10,9 @@ func _on_play_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
-
+	
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
+	
 func _on_quit_pressed() -> void:
 	get_tree().quit()
