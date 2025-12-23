6a3bb47c5bf4091b7702a30a2ca33c7f8c02f239
VeryVeryCoolName <teo.circiumarescu@gmail.com>
2025-12-23 16:22:12 +0200

    oajsodaj

--- Scripts/outro.gd
+++ Scripts/outro.gd
@@ -9,6 +9,7 @@ extends Node
 
 func _ready():
 	AudioManager.stop()
+	
 	if animationPlayer1:
 		animationPlayer1.play("fade_in1")
 	if animationPlayer2:
@@ -20,4 +21,4 @@ func _ready():
 	if animationPlayer5:
 		animationPlayer5.play("fade_in5")
 	await get_tree().create_timer(introtimer).timeout
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
