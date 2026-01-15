a5c50c83c4b774e169a477e557ed5e6d8a37905f
VeryVeryCoolName <teo.circiumarescu@gmail.com>
2026-01-15 21:51:33 +0200

    v4 e aproape gata
    am scos godot boot screenul + acum se poate sa se intre in main menu din setari

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -35,3 +35,8 @@ func _on_resume_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_menu_pressed() -> void:
+	resume()
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
